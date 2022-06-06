230226c7ce16e89006a220ade3595901c8f7cf31
Stas Vanchugov <s.vanchugov@4walls.team>
2022-06-06 09:49:33 +0500

    Изменено именование.

--- database/factories/ApartmentsFactory.php
+++ database/factories/ApartmentsFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Apartments>
  */
-class ApartmentFactory extends Factory
+class ApartmentsFactory extends Factory
 {
     /**
      * Define the model's default state.
